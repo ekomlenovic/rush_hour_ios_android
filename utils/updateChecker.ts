c3f9088d195e206401b4572deaa4fda2135cd92d
ekomlenovic <emilien.komlenovic@gmail.com>
2026-03-16 00:17:43 +0100

    test new pipeline

--- utils/updateChecker.ts
+++ utils/updateChecker.ts
@@ -1,6 +1,10 @@
+import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
-const SOURCE_URL = 'https://raw.githubusercontent.com/ekomlenovic/rush_hour_ios_android/refs/heads/main/sidestore-source.json';
+const SOURCE_BASE_URL = 'https://raw.githubusercontent.com/ekomlenovic/rush_hour_ios_android/refs/heads/main';
+const SOURCE_URL = Platform.OS === 'ios' 
+  ? `${SOURCE_BASE_URL}/sidestore-source.json`
+  : `${SOURCE_BASE_URL}/android-source.json`;
 
 export interface UpdateInfo {
   isUpdateAvailable: boolean;
